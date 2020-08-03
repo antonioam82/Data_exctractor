bf41c090b10065994ec3d65b0d2cd734b2d630f0
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-08-03 16:31:54 +0200

    Update data_extractor.py

--- data_extractor.py
+++ data_extractor.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import tkinter.scrolledtext as scrolledtext
+from tkinter import filedialog
 from PIL import Image
 from PIL.ExifTags import TAGS
 
@@ -11,10 +12,34 @@ class App:
 
         self.display = scrolledtext.ScrolledText(self.ventana,bg="black",fg="green",width=65,height=20)
         self.display.pack(side=TOP)
-        self.btn_search = Button(self.ventana,text="SEARCH FILE",bg="orange",width=30)
+        self.btn_search = Button(self.ventana,text="SEARCH FILE",bg="orange",width=30,command=self.open_file)
         self.btn_search.pack(side=TOP)
         
         self.ventana.mainloop()
+
+    def open_file(self):
+        file = filedialog.askopenfilename(initialdir="/",title="SELECT FILE",
+                                        filetypes=(("jpg files","*.jpg"),("png files","*.jpg")))
+        if file is not None:
+            self.extract_data(file)
+
+    def extract_data(self,f):
+        image = Image.open(f)
+        exifdata = image._getexif()
+
+        if exifdata is not None:
+            for tag_id in exifdata:
+                tag = TAGS.get(tag_id, tag_id)
+                data = exifdata.get(tag_id)
+                if isinstance(data, bytes):
+                    data = data.decode('UTF8','replace')
+                self.display.insert(END,'{} {}'.format(tag,data)+"\n")
+        else:
+            self.display.insert(END,'NO DATA')
+
+    
+            
+            
 
 if __name__=="__main__":
     App()
