af708ece86434398aa6d6d04084078affcddab07
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-08-11 11:54:08 +0200

    Update EXIF_data_viewerB.py

--- EXIF_data_viewerB.py
+++ EXIF_data_viewerB.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from tkinter import *
+from tkinter import messagebox
 import re
 import tkinter.scrolledtext as scrolledtext
 from tkinter import filedialog
@@ -33,19 +34,20 @@ class App:
         self.file = filedialog.askopenfilename(initialdir="/",title="SELECT FILE",
                                         filetypes=(("jpeg files","*.jpg"),("all files","*.*")))
         if self.file != "":
-            self.file_label.configure(text=(self.file).split("/")[-1])
+            self.filename = (self.file).split("/")[-1]
+            self.file_label.configure(text=self.filename)
             self.extract_data(self.file)
 
     def extract_data(self,f):
         self.display.delete('1.0',END)
         try:
             image = Image.open(f)
-            exifdata = image._getexif()
-            if exifdata is not None:
+            self.exifdata = image._getexif()
+            if self.exifdata is not None:
                 self.display.insert(END,"-"*26+"METADATA INFO"+"-"*26+"\n")
-                for tag_id in exifdata:
+                for tag_id in self.exifdata:
                     tag = TAGS.get(tag_id, tag_id)
-                    data = exifdata.get(tag_id)
+                    data = self.exifdata.get(tag_id)
                     if isinstance(data, bytes):
                         data = data.decode('UTF8','replace')
                     try:
@@ -55,15 +57,16 @@ class App:
                         self.display.insert(END,f"{tag:26}: {data}"+"\n")
                 self.display.insert(END,"-"*65)
             else:
-                self.display.insert(END,'NO DATA.')
+                self.display.insert(END,'NO DATA')
         except:
-            self.display.insert(END,'ERROR.')
+            self.display.insert(END,'ERROR')
 
     def remove(self):
-        if self.file != "":
-            piexif.remove(self.file)
-            self.extract_data(self.file)
+        if self.file != "" and self.exifdata is not None:
+            msgBox = messagebox.askquestion('Are you sure?','Do you want to remove all EXIF data from {}?'.format(self.filename))
+            if msgBox == 'yes':
+                piexif.remove(self.file)
+                self.extract_data(self.file)
 
-    
 if __name__=="__main__":
     App()
