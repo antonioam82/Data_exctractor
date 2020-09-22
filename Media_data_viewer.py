4fd6ba1daab6ec0b34f1f2d4c4df38236ca17314
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-09-22 15:26:04 +0200

    Update Media_data_viewer.py

--- Media_data_viewer.py
+++ Media_data_viewer.py
@@ -14,10 +14,19 @@ class Visualizer:
         self.file_label.pack(side=TOP)
         self.display = scrolledtext.ScrolledText(self.ventana,bg="blue",fg="white",width=67,height=23)
         self.display.pack(side=TOP)
-        self.btn_search = Button(self.ventana,text="SEARCH FILE",bg="light blue",width=30)
+        self.btn_search = Button(self.ventana,text="SEARCH FILE",bg="light blue",width=30,command=self.open_file)
         self.btn_search.pack(side=TOP)       
 
         self.ventana.mainloop()
+
+    def open_file(self):
+        self.file = filedialog.askopenfilename(initialdir="/",title="SELECT FILE",
+                                        filetypes=(("mp4 files","*.mp4"),("all files","*.*")))
+        if self.file != "":
+            self.filename = (self.file).split("/")[-1]
+            self.file_label.configure(text=self.filename)
+            print(self.filename)
+            #self.extract_data(self.file)
 
 if __name__=="__main__":
     Visualizer()
